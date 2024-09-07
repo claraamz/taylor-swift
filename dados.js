744083a77e6668081dc8aa0fde129e0e6795fa36
claraamz <clara.amazonas08@gmail.com>
2024-09-07 00:23:49 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,54 @@
+let dados = [
+    {
+        titulo: "Biografia",
+        descricao: "Taylor Swift nasceu em 13 de dezembro de 1989, em Reading, Pennsylvania. Desde cedo, demonstrou um talento natural para a música, influenciada por sua família e pelos gêneros musicais que ouviu. Aos 14 anos, ela se mudou para Nashville, Tennessee, para seguir uma carreira na música country. A persistência e o talento de Taylor foram recompensados quando ela assinou um contrato com a Big Machine Records e lançou seu álbum de estreia homônimo em 2006. Desde então, sua carreira só cresceu, consolidando-se como uma das artistas mais influentes e bem-sucedidas da indústria musical.",
+        link: "https://pt.wikipedia.org/wiki/Taylor_Swift",
+        tags: "biografia bio "
+    },
+    {
+        titulo: "Discografia",
+        descricao:`
+      <ul>
+        <li>Taylor Swift (2006)</li>
+        <li>Fearless (2008)</li>
+        <li>Speak Now (2010)</li>
+        <li>Red (2012)</li>
+        <li>1989 (2014)</li>
+        <li>Reputation (2017)</li>
+        <li>Lover (2019)</li>
+        <li>Folklore (2020)</li>
+        <li>Evermore (2020)</li>
+        <li>Midnights (2022)</li>
+        <li>The Tortured Poets Department (2024)</li>
+      </ul>`,
+        link: "https://pt.wikipedia.org/wiki/Discografia_de_Taylor_Swift",
+        tags: "discografia músicas musicas albums álbums"
+    },
+    {
+        titulo: "Evolução Musical",
+        descricao: "Taylor Swift começou sua carreira no gênero country, com um som jovem e influenciado por artistas como Shania Twain e Dolly Parton. Com o tempo, ela começou a experimentar outros estilos e a expandir seu som. O álbum Red marcou uma transição importante, misturando elementos de pop e rock. Finalmente, com 1989, ela fez uma transição completa para o pop, abraçando uma sonoridade mais eletrônica e moderna. Sua habilidade de adaptar-se e inovar a cada álbum mantém seu trabalho fresco e relevante.",
+        link: "https://youtu.be/Jb2stN7kH28?feature=shared",
+        tags: "evolução musical carreira"
+    },
+    {
+        titulo: "Tours e Shows",
+        descricao: `
+      <ul>
+        <li>Fearless Tour (2009-2010)</li>
+        <li>Speak Now World Tour (2011-2012)</li>
+        <li>The Red Tour (2013-2014)</li>
+        <li>The 1989 World Tour (2015)</li>
+        <li>Reputation Stadium Tour (2018)</li>
+        <li>Lover Fest (cancelado em 2020)</li>
+        <li>The Eras Tour (2023-2024)</li>
+      </ul>`,
+        link: "https://www.taylorswift.com/tour/",
+        tags: "tour show shows "
+    },
+    {
+        titulo: "Impacto Cultural",
+        descricao: "Taylor Swift não é apenas uma artista de sucesso; ela tem um impacto profundo na cultura pop. Seus álbuns frequentemente abordam temas universais, como amor, crescimento pessoal e empoderamento, ressoando com uma ampla audiência. Além disso, ela influenciou tendências de moda e a forma como os artistas interagem com seus fãs. Suas decisões sobre como lidar com questões públicas, como a política e a privacidade, também demonstram sua habilidade em moldar o discurso cultural. Taylor Swift é uma das figuras mais proeminentes da cultura pop contemporânea, com um legado que vai além da música.",
+        link: "https://youtu.be/7Gbg6Z70J7E?feature=shared",
+        tags: "artista impacto cultural"
+    } 
+];
